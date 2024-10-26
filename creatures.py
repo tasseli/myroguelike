f5e129df7ff825289097240ef1a567b04fba277a
Mikael Merenparras <tassel@iki.fi>
2024-10-26 13:13:37 +0300

    Getting ready to pull data directly from creature properties

--- creatures.py
+++ creatures.py
@@ -1,6 +1,9 @@
 # creatures.py
 
 class Player:
+    sign = "@"
+    id = 2
+
     def __init__(self):
         self.position = [3, 6]
 
@@ -8,6 +11,9 @@ class Player:
         return self.position
 
 class Orc:
+    sign = "o"
+    id = 3
+
     def __init__(self):
         self.position = [62, 30]
 
