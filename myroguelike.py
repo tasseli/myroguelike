653c22fcb1cd7307a7ccaeff1f6f34243f39372a
Mikael Merenparras <tassel@iki.fi>
2024-11-30 16:19:59 +0200

    Changing logic to not try to merge different death notes into one message. Bug with bonus deaths fixed.

--- myroguelike.py
+++ myroguelike.py
@@ -29,14 +29,13 @@ game_map = Map()
 
 def check_deaths(game_map, moves_bool):
     dying = []
-    death_note = ""
     if moves_bool:
         for i in range(1, len(game_map.creatures)):                     # don't move the player in a loop
-            death_note += game_map.move_moodily(i, game_map.creatures)
+            death_note = game_map.move_moodily(i, game_map.creatures)
             if death_note != "":
                 print("Death note works: " + death_note)
     for i in range(0, len(game_map.creatures)):
-        death_note += game_map.creatures[i].check_death()
+        death_note = game_map.creatures[i].check_death()
         if death_note != "":
             if i == 0:
                 quit_app("You died!")
@@ -47,8 +46,6 @@ def check_deaths(game_map, moves_bool):
         death_location = game_map.creatures[dying[deaths-i-1]].get_position()
         game_map.my_map[death_location[0]][death_location[1]] = OPEN_SPACE
         game_map.creatures.pop(dying[deaths-i-1])
-    if death_note != "":
-        print("Full death note: " + death_note)
 
 # Main loop
 
