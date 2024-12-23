412c80b1831d0343dc8417802296e4c8708e9d28
Mikael Merenparras <tassel@iki.fi>
2024-12-23 11:20:28 +0200

    Moved check_deaths under death.py

--- myroguelike.py
+++ myroguelike.py
@@ -2,6 +2,7 @@
 
 import pygame
 import sys
+from death import check_deaths
 from map import (Map, 
 MAP_WIDTH, MAP_HEIGHT, TILE_SIZE,
 OPEN_SPACE, WALL, PLAYER, ORC)
@@ -27,28 +28,6 @@ def quit_app(reason):
 # Initialize an empty map
 game_map = Map()
 
-def check_deaths(game_map, moves_bool):
-    dying = []
-    death_notes = []
-    if moves_bool:
-        for i in range(1, len(game_map.creatures)):                     # don't move the player in a loop
-            death_note = game_map.move_moodily(i, game_map.creatures)
-            if death_note != "":
-                death_notes.append(death_note)
-    for i in range(0, len(game_map.creatures)):
-        death_note = game_map.creatures[i].check_death()
-        if death_note != "":
-            if i == 0:
-                quit_app("You died!")
-            dying.append(i)
-            death_notes.append(death_note)
-    deaths = len(dying)
-    for i in range(0, deaths):
-        death_location = game_map.creatures[dying[deaths-i-1]].get_position()
-        game_map.my_map[death_location[0]][death_location[1]] = OPEN_SPACE
-        game_map.creatures.pop(dying[deaths-i-1])
-    return death_notes
-
 # Main loop
 
 messages = ["Welcome to Myr!", "Move around with arrows or numpad.", "Moving towards an 'o'rc attacks.", "Good luck!"]
@@ -56,7 +35,7 @@ while True:
     # Clear the screen
     screen.fill(GRAY)
 
-# print message log (previous)
+# print message log
     if len(messages) > 0:
         if len(messages) < 4:
             for i in range(4 - len(messages)):
