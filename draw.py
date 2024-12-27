42b9bd006adbc881f4e1d622224745d5367fe048
Mikael Merenparras <tassel@iki.fi>
2024-12-27 17:26:16 +0200

    Adding files

---
+++ draw.py
@@ -0,0 +1,36 @@
+# draw.py
+
+from map import (WHITE, GRAY, DARK_GRAY, BLACK)
+from map import (MAP_WIDTH, MAP_HEIGHT, TILE_SIZE)
+from map import (OPEN_SPACE, WALL, PLAYER, ORC)
+
+def render_message_log(messages, pygame, screen, font):
+    if len(messages) > 0:
+        if len(messages) < 4:
+            for i in range(4 - len(messages)):
+                messages.append("")
+                #now there are 4 strings in message
+        string_index = 0
+        for a_string in messages:
+            draw_and_blit_char(pygame, screen, font, " ", 0, MAP_HEIGHT + string_index, GRAY)
+            for i in range(0, len(a_string)):
+                draw_and_blit_char(pygame, screen, font, a_string[i], 1 + i, MAP_HEIGHT + string_index, DARK_GRAY)
+            for j in range(len(a_string), MAP_WIDTH):
+                draw_and_blit_char(pygame, screen, font, " ", j, MAP_HEIGHT + string_index, GRAY)
+            string_index += 1
+
+def render_map(game_map, pygame, screen, font):    
+    y = 0
+    for x in range(0, MAP_WIDTH):
+        for y in range(0, MAP_HEIGHT):
+            if game_map.get_sign(x,y) == OPEN_SPACE:
+                draw_and_blit_char(pygame, screen, font, game_map.get_sign(x,y), x, y, GRAY)
+            else:
+                draw_and_blit_char(pygame, screen, font, game_map.get_sign(x,y), x, y, DARK_GRAY)
+
+def draw_and_blit_char(pygame, screen, font, my_char, x, y, color):
+    pygame.draw.rect(screen, color, (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
+    # Render the my_char (e.g. '#' / '@') character on top of the background color
+    text_surface = font.render(my_char, True, BLACK)
+    text_rect = text_surface.get_rect(center=(x * TILE_SIZE + TILE_SIZE // 2, y * TILE_SIZE + TILE_SIZE // 2))
+    screen.blit(text_surface, text_rect)
---
+++ loop_utils.py
@@ -0,0 +1,36 @@
+# loop_utils.py
+
+import sys
+from keyboard import read_moves
+from death import check_deaths
+
+def quit_app(reason, pygame):
+    print(reason)
+    pygame.quit()
+    sys.exit()    
+
+def handle_events(pygame, game_map, messages):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit_app("event.type == quit")
+        if event.type == pygame.KEYDOWN:
+            new_position = game_map.player.get_position().copy()
+            outcome = read_moves(new_position, event.key, pygame)
+            if outcome == "q":
+                quit_app("q or x pressed", pygame)
+            elif outcome == "o":
+#               An unhandled key was pressed
+                pass
+            elif outcome == "m" or outcome == "s":
+#               Move key or stand still key
+                messages = [""]
+                death_notes1 = []
+                if game_map.move_to(0, new_position, game_map.creatures): # 0 refers to the player
+                    death_notes1 = check_deaths(game_map, False)   # check if player killed anyone
+                death_notes2 = check_deaths(game_map, True)    # have all creatures move and check if they killed anyone
+                if len(death_notes1) > 0:
+                    messages = death_notes1
+                if len(death_notes1) > 0:
+                    messages += death_notes2
+    return messages
+
